4bec1170c26eec10d46ba3d32ac11194ac1adad1
Rihards Edmunds Cerps <goldminelv@gmail.com>
2022-12-04 18:29:26 +0200

    Remvoe typescript and configure babel

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import axios from 'axios'
+import router from './router'
+
+
+const app = createApp(App).use(router);
+app.config.globalProperties.$http = axios;
+app.config.globalProperties.$filters = {
+    formatCurrency(number) {
+        return Intl.NumberFormat('lv-LV', { style: 'currency', currency: 'EUR' }).format(number);
+    }
+}
+
+app.mount('#app');
